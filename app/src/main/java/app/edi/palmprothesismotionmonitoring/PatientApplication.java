8a6ff900cfa4a15a96e37915de20aba5af646ae9
Ričards Cacurs <rcacurs@gmail.com>
2015-12-21 09:42:31 +0200

    creating settings and application classes

---
+++ app/src/main/java/app/edi/palmprothesismotionmonitoring/PatientApplication.java
@@ -0,0 +1,10 @@
+package app.edi.palmprothesismotionmonitoring;
+
+import android.app.Application;
+
+/**
+ * Created by Richards on 21.12.2015..
+ */
+public class PatientApplication extends Application {
+
+}
