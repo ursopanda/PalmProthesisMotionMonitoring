8f4f75d590a7c6594884b32e70d37bf2c7e05b91
Ričards Cacurs <rcacurs@gmail.com>
2015-12-22 10:23:35 +0200

    implemented bt connection and data stream

--- app/src/main/java/app/edi/palmprothesismotionmonitoring/MainActivity.java
+++ app/src/main/java/app/edi/palmprothesismotionmonitoring/MainActivity.java
@@ -3,18 +3,24 @@ package app.edi.palmprothesismotionmonitoring;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import lv.edi.BluetoothLib.BluetoothService;
+
 public class MainActivity extends AppCompatActivity {
     private PatientApplication application;
     private final int REQUEST_ENABLE_BT = 1;
+    private MenuItem btConnect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,12 +39,39 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(intent, REQUEST_ENABLE_BT);
         }
+        if(application.btService==null) {
+            application.btService = new BluetoothService(application.sensors, application.BATTERY_PACKET);
+            application.btService.registerBluetoothEventListener(application);
+        }
     }
 
     @Override
+    protected void onResume(){
+        super.onResume();
+        application.mainActivity = this;
+    }
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+        Log.d("MAIN_ACTIVITY", "CREATING MENU ");
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        btConnect = menu.findItem(R.id.action_bt);
+        if(application.btService!=null){
+            Log.d("MAIN_ACTIVITY", "BT SERVICE STATUS "+application.btService.isConnected()+" "+application.btService.isConnecting());
+            if(application.btService.isConnected()){
+                btConnect.setIcon(R.drawable.check);
+                return true;
+            }
+            if(application.btService.isConnecting()){
+                btConnect.setIcon(R.drawable.loading);
+                return true;
+            }
+            if(!(application.btService.isConnected())){
+                btConnect.setIcon(R.drawable.not);
+                return true;
+            }
+        }
+        Log.d("MAIN_ACTIVITY", btConnect.toString());
         return true;
     }
 
@@ -56,6 +89,22 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
 
+        if (id == R.id.action_bt){
+            //if is connecting block this button
+            if(application.btService.isConnecting()){
+                return false;
+            }
+            if(!(application.btService.isConnected())){
+                if(application.btDevice == null){
+                    Toast.makeText(this, getString(R.string.must_select_bt_device), Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+                application.btService.connectDevice(application.btDevice);
+            } else{
+                application.btService.disconnectDevice();
+            }
+        }
+
         return super.onOptionsItemSelected(item);
     }
 
@@ -66,5 +115,29 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         }
+    }
+
+    public void btConnected(){
+        runOnUiThread(new Runnable() {
+            public void run() {
+                btConnect.setIcon(R.drawable.check);
+            }
+        });
+    }
+
+    public void btDisconnected(){
+        runOnUiThread(new Runnable(){
+            public void run(){
+                btConnect.setIcon(R.drawable.not);
+            }
+        });
+    }
+
+    public void btConnecting(){
+        runOnUiThread(new Runnable(){
+            public void run(){
+                btConnect.setIcon(R.drawable.loading);
+            }
+        });
     }
 }
--- app/src/main/java/app/edi/palmprothesismotionmonitoring/PatientApplication.java
+++ app/src/main/java/app/edi/palmprothesismotionmonitoring/PatientApplication.java
@@ -4,16 +4,29 @@ import android.app.Application;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.SharedPreferences;
+import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.util.Log;
+
+import java.util.Vector;
+
+import lv.edi.BluetoothLib.BluetoothEventListener;
+import lv.edi.BluetoothLib.BluetoothService;
+import lv.edi.SmartWearProcessing.Sensor;
 
 /**
  * Created by Richards on 21.12.2015..
  */
-public class PatientApplication extends Application implements SharedPreferences.OnSharedPreferenceChangeListener{
+public class PatientApplication extends Application implements SharedPreferences.OnSharedPreferenceChangeListener, BluetoothEventListener {
     private SharedPreferences sharedPrefs;
     BluetoothDevice btDevice;
     BluetoothAdapter btAdapter;
+    BluetoothService btService;
+    Vector<Sensor> sensors = new Vector<Sensor>();
+    static final int NUM_SENSORS=4;
+    static final int BATTERY_PACKET=4;
+    MainActivity mainActivity;
+    Handler uiHandler;
     @Override
     public void onCreate(){
         super.onCreate();
@@ -29,6 +42,11 @@ public class PatientApplication extends Application implements SharedPreferences
             btDevice = null;
         } else{
             btDevice = btAdapter.getRemoteDevice(btDeviceAddress);
+        }
+
+        // allocate memory for sensor data
+        for(int i=0; i<NUM_SENSORS; i++){
+            sensors.add(new Sensor(i, true));
         }
         Log.d("APPLICATION", "bt device " + btDevice.getName());
     }
@@ -46,6 +64,19 @@ public class PatientApplication extends Application implements SharedPreferences
             }
             Log.d("APPLICATION", "bt device " + btDevice.getName());
         }
+    }
+
+    @Override
+    public void onBluetoothDeviceConnecting(){
+        mainActivity.btConnecting();
+    }
+    @Override
+    public void onBluetoothDeviceConnected(){
+        mainActivity.btConnected();
+    }
+    @Override
+    public void onBluetoothDeviceDisconnected(){
+        mainActivity.btDisconnected();
     }
 
 }
