7f1aec518cd24f918e9aaf578af2d0f87c828dc6
Ričards Cacurs <rcacurs@gmail.com>
2016-02-02 17:03:55 +0200

    implementing calibration activity

--- app/src/main/java/app/edi/palmprothesismotionmonitoring/CalibrationActivity.java
+++ app/src/main/java/app/edi/palmprothesismotionmonitoring/CalibrationActivity.java
@@ -2,15 +2,51 @@ package app.edi.palmprothesismotionmonitoring;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import org.ejml.data.DenseMatrix64F;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.Vector;
+
+import lv.edi.SmartWearProcessing.SensorDataProcessing;
 
 /**
  * Created by richards on 16.2.2.
  */
 public class CalibrationActivity extends AppCompatActivity {
-
+    PatientApplication application;
+    private Vector<DenseMatrix64F> data;
+    Timer dataAcquisitionTimer;
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.calibration_layout);
 
+        application = (PatientApplication)getApplication();
+    }
 
+    public void onClickCalibrate(View button){
+        ToggleButton tb = (ToggleButton) button;
+        if(tb.isChecked()) {
+            if (application.btService.isConnected()) {
+                data = new Vector<DenseMatrix64F>();
+                dataAcquisitionTimer = new Timer();
+
+                dataAcquisitionTimer.scheduleAtFixedRate(new TimerTask(){
+                    public void run() {    // fetch data
+
+                    }} ,0 , 100);
+
+            } else {
+                Toast.makeText(this, "Connect to bt device first!", Toast.LENGTH_SHORT).show();
+                ((ToggleButton) button).setChecked(false);
+            }
+        } else{
+            dataAcquisitionTimer.cancel();
+        }
     }
 }
--- app/src/main/java/app/edi/palmprothesismotionmonitoring/CalibrationView.java
+++ app/src/main/java/app/edi/palmprothesismotionmonitoring/CalibrationView.java
@@ -1,7 +1,68 @@
 package app.edi.palmprothesismotionmonitoring;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+
+import org.ejml.data.DenseMatrix64F;
+
+import java.util.Vector;
+
 /**
  * Created by richards on 16.2.2.
  */
-public class CalibrationView {
+public class CalibrationView extends View {
+
+    private Paint framePaint;
+    private Paint pointPaint;
+    private Vector<DenseMatrix64F> data;
+    private float norm = 364;
+
+    public CalibrationView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        framePaint = new Paint();
+        framePaint.setColor(Color.BLACK);
+        framePaint.setStyle(Paint.Style.STROKE);
+        framePaint.setStrokeWidth(3);
+        framePaint.setPathEffect(new DashPathEffect(new float[]{10, 20}, 0));
+
+        pointPaint = new Paint();
+        pointPaint.setColor(Color.BLUE);
+        pointPaint.setStyle(Paint.Style.FILL);
+
+        data = new Vector<DenseMatrix64F>();
+        double[] dt = {0, 200, 0};
+        data.add(new DenseMatrix64F(1,3, true, dt));
+    }
+
+    /**
+     * draw callback
+     * @param canvas
+     */
+    protected void onDraw(Canvas canvas){
+
+
+        int width = canvas.getWidth()-1;
+        int height = canvas.getHeight()-1;
+
+        int offsetX = width/2;
+        int offsetY = height/2;
+
+        int radius = Math.min(width, height)/2;
+        canvas.drawCircle(width/2, height/2, radius, framePaint);
+        if(data!=null){
+            for(DenseMatrix64F point : data){
+                float rawX = (float)point.get(0,0);
+                float rawY = (float)point.get(0,1);
+
+                canvas.drawCircle(offsetX+(rawX/norm)*radius, offsetY+(rawY/norm)*radius, 10, pointPaint);
+            }
+        }
+    }
 }
